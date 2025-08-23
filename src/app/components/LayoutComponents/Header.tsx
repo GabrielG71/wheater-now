85ebdbf1cf4bb3482c13501e0260833d35b2d184
Gabriel Gonçalves <gabrielgoncalves2981@gmail.com>
2025-08-23 14:10:46 -0300

    Changing Name

--- src/app/components/LayoutComponents/Header.tsx
+++ src/app/components/LayoutComponents/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
         <div className="flex items-center justify-center space-x-3">
           <span className="text-4xl">☀️</span>
           <h1 className="text-2xl md:text-3xl font-bold text-white tracking-wide">
-            Weather Now
+            Weathering With You
           </h1>
         </div>
       </div>
